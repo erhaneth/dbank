45fa3cee239da915a35dee9b266ba91ba6658b72
“erhaneth” <gumusucb21@gmail.com>
2022-07-15 15:59:47 -0700

    Fixed toggle bug

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ function NavBar() {
         <div className="md:flex[0.5] flex-initial justify-center"> 
          <img  src={logo} alt="logo" className="h-36 w-48 cursor-pointer" />
         </div>
-        <ul className="text-black md:flex hidden flex-row  justify-between items-center flex-initial ">
+        <ul className="text-black md:flex hidden list-none flex-row  justify-between items-center flex-initial ">
             {/* map items and pass to navbar */}
             {["Transactions", "Wallet"].map((item, index) =>(
                 <NavbarItem key={item + index} title={item}  />
@@ -29,24 +29,24 @@ function NavBar() {
                 Login
             </li>
         </ul>
-        <div className="flex relative"> 
-          {toggle 
-          ?<AiOutlineClose fontSize={28} className="text-black md:hidden cursor-pointer" onClick={() => setToggle(false)}/> 
-          :<HiMenuAlt4 fontSize={28} className="text-black md:hidden cursor-pointer" onClick={() => setToggle(true )}/>
-          {toggle && (
-            <ul>
-              <li className="text-xl w-full my-2">
-                <AiOutlineClose onClick={() => setToggle(false)} />
-              </li>
-              {["Transactions", "Wallet"].map((item, index) =>(
-                <NavbarItem key={item + index} title={item} classProps="my-2 text-lg"  />
-
-            )) }
-            </ul>
-
+        <div className="flex relative">
+        {!toggle && (
+          <HiMenuAlt4 fontSize={28} className="text-black md:hidden cursor-pointer" onClick={() => setToggle(true)} />
         )}
-          
-        </div>
+        {toggle && (
+          <AiOutlineClose fontSize={28} className="text-black md:hidden cursor-pointer" onClick={() => setToggle(false)} />
+        )}
+        {toggle && (
+          <ul
+            className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen bg-gray-200 shadow-lg md:hidden list-none flex flex-col justify-start items-end blue-glassmorphism"
+          >
+            <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggle(false)} /></li>
+            {["Transactions", "Wallets"].map(
+              (item, index) => <NavbarItem key={item + index} title={item} classProps="my-2 text-lg" />,
+            )}
+          </ul>
+        )}
+      </div>
      </nav>
     )
 }
