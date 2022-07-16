a19349ee28de3149ee91fa2ea9de088cd4b0b9b0
“erhaneth” <gumusucb21@gmail.com>
2022-07-16 11:17:00 -0700

    Fixed the bug and implement inputs

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -3,19 +3,23 @@ import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
 // import { loader } from './'
-const commonStyles ="min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+const commonStyles =
+  "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
-const Input = ({placeholder}) => {
-    
-     <input
+  const Input = ({ placeholder, name, type, value, handleChange }) => (
+    <input
       placeholder={placeholder}
-     />
-    
-}
+      type={type}
+      step="0.0001"
+      value={value}
+      onChange={(e) => handleChange(e, name)}
+      className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm blue-glassmorphism"
+    />
+  )
 function Welcome() {
   const connectWallet = () => {
     window.location.href = "https://metamask.io/";
-  };
+  }
 
   return (
     <div className="flex w-full justify-center items-center">
@@ -23,7 +27,7 @@ function Welcome() {
         <div className="flex flex-1  justify-start flex-col md:mr-10">
           <button
             type="button"
-            onclick={connectWallet}
+            onClick={connectWallet}
             className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#5867b2]"
           >
             <p className="text-white text-base font-bold">Connect Wallet</p>
@@ -34,29 +38,60 @@ function Welcome() {
           <div className="grid sm:grid-cols-3 grid-cols-3 w-full mt-10">
             <div className={`rounded-tl-2xl ${commonStyles}`}>Uncensorable</div>
             <div className={commonStyles}>Secure</div>
-            <div className={`rounded-tr-2xl ${commonStyles}`}>Persistent</div>
-            <div className={`rounded-bl-2xl ${commonStyles}`}>Transparent</div>
+            <div className={`sm:rounded-tr-2xl ${commonStyles}`}>
+              Persistent
+            </div>
+            <div className={`sm:rounded-bl-2xl ${commonStyles}`}>
+              Transparent
+            </div>
             <div className={commonStyles}>Decentralized</div>
             <div className={`rounded-br-2xl ${commonStyles}`}>Peer-to-Peer</div>
           </div>
         </div>
         <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
-          <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card">
-            <div className="flex flex-col justify-between w-full h-full">
+          <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
+            <div className="flex justify-between flex-col  w-full h-full">
               <div className="flex justify-between items-start">
                 <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center ">
                   <SiEthereum fontSize={21} color="white" />
                 </div>
                 <BsInfoCircle fontSize={17} color="white" />
               </div>
-              <p className="text-white font-light text-sm">Address</p>
-              <p className="text-white font-semibold text-lg mt-1">Ethereum</p>
+              <div>
+                <p className="text-white font-light text-sm">Address</p>
+                <p className="text-white font-semibold text-lg mt-1">
+                  Ethereum
+                </p>
+              </div>
             </div>
           </div>
+          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+            <Input
+              placeholder="Address to"
+              name="addressTo"
+              type="text"
+              handelChange={() => {}}
+            />
+            <Input
+              placeholder="Amount (ether)"
+              name="amount"
+              type="number"
+              handelChange={() => {}}
+            />
+            <Input
+              placeholder="Keyword (Gifhy)"
+              name="keyword"
+              type="text"
+              handelChange={() => {}}
+            />
+            <Input
+              placeholder="Enter your message"
+              name="message"
+              type="text"
+              handelChange={() => {}}
+            />
+          </div>
         </div>
-      </div>
-      <div className="p-5 sm:w-96 flex flex-col justify-start items-center blue-glassmorphism">
-
       </div>
     </div>
   );
