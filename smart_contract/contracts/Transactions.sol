c4a1d59665e1c7afcdc71c4ee94cb211cab2a6bd
“erhaneth” <gumusucb21@gmail.com>
2022-07-16 19:30:28 -0700

    implement transactions

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+// transacrion class for the  contract
+contract Transactions {
+    // a variable to store transactions of the contract
+    uint256 transactionCount;
+    //a function will be called later on
+    event Transafer( 
+        address from, 
+        address recevier, 
+        uint amount, 
+        string message, 
+        uint256 timestamp, 
+        string keyword 
+        );
+    // a constructor to initialize the contract - like object constructor
+    // type of property, name of the property
+    struct TransferStruct {
+        address sender;
+        address receiver;
+        uint256 amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+    //variable transactions  is an array of TransferStruct
+    TransferStruct[] transactions;
+
+    function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword )public {
+       //incremet the transaction counter
+       transactionCount += 1;
+       transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+
+       emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
+    }
+    // it returns array of TransferStruct from memory
+    function getAllTransactions() public view returns (TransferStruct[] memory) {
+        return transactions;
+    }
+
+    
+    function getTransactionCount() public view returns (uint256) {
+        return transactionCount;
+    }
+
+    
+}
