71e0b3ce7a81df9b9a15a7edd4043cd88d6268ca
“erhaneth” <gumusucb21@gmail.com>
2022-07-18 14:29:19 -0700

    Render transactions

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,7 +1,35 @@
 import React, { useContext } from "react";
-
+import { shortAddress } from "../utils/shortAddress";
 import { TransactionContext } from "../context/TransactionContext";
-// import dummyData from "../utils/dummyData";
+import dummyData from "../utils/dummyData";
+
+const TransactionCard = ({
+  addressTo,
+  addressFrom,
+  timestamp,
+  message,
+  keyboard,
+  amount,
+  url,
+}) => {
+  return (
+    <div className="bg-gray-400 m-4 flex flex-1 2xl:min-w-[450px] 2xl:max-w-[500px] sm:min-w-[270px] sm:max-w-[300px] flex-col p-3 rounded-md hover:shadow-2xl">
+        <div className="flex flex-col items-center w-full mt-3">
+        <div className="display-flex justify-start w-full mb-6 p-2">
+        <a href={`https://goerli.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
+        <p className="text-white text-base">From: {shortAddress(addressFrom)}</p>
+        </a>
+        <a href={`https://goerli.etherscan.io/address/${addressTo}`} target="_blank" rel="noreferrer">
+        <p className="text-white text-base">From: {shortAddress(addressTo)}</p>
+        </a>
+        </div>
+        </div>
+            
+
+    </div>
+  );
+};
+
 function Transactions() {
   const { currentAccount } = useContext(TransactionContext);
   return (
@@ -14,6 +42,11 @@ function Transactions() {
             Connect your account to see recent transactions
           </h3>
         )}
+        <div className="flex flex-wrap justify-center items-center mt-10">
+          {dummyData.reverse().map((transaction, index) => (
+            <TransactionCard key={index} {...transaction} />
+          ))}
+        </div>
       </div>
     </div>
   );
