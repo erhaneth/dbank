861704b2297b145273057fef8cc231e92aace947
“erhaneth” <gumusucb21@gmail.com>
2022-07-18 11:16:04 -0700

    Send eth to another address succesuflly

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -10,16 +10,18 @@ const getEthereumContract = () => {
   const provider = new ethers.providers.Web3Provider(ethereum);
   const signer = provider.getSigner();
   const contract = new ethers.Contract(contractAddress, contractABI, signer);
-  // return contract;
-  console.log({
-    provider,
-    signer,
-    contract,
-  });
+  return contract;
+//   console.log({
+//     provider,
+//     signer,
+//     contract,
+//   });
 };
 export const TransactionProvider = ({ children }) => {
   const [currentAccount, setCurrentAccount] = useState("");
   const [ formData, setFormData ] = useState({ addressTo: "", amount: "", message: "", keyword: "" });
+  const [ isLoading, setIsLoading ] = useState(false);
+  const [ transactionCount, setTransactionCount ] = useState(localStorage.getItem("transactionCount"));
 
   const handleChange = (e, name) => {
     setFormData({ ...formData, [name]: e.target.value });
@@ -30,7 +32,7 @@ export const TransactionProvider = ({ children }) => {
     try {
         
         if (!ethereum) return;
-        console.log("Please connect to a wallet");
+        // console.log("Please install MetaMask");
         const accounts = await ethereum.request({ method: "eth_accounts" });
         // console.log( accounts );
         if (accounts.length > 0) {
@@ -60,13 +62,44 @@ export const TransactionProvider = ({ children }) => {
   useEffect(() => {
     checkIfWalletConnected();
   }, []);
-   //   sending and storing transaction
+   //sending from one to and another and storing transaction
   const sendTransaction = async (to, value) => {
     try {
         if (!ethereum) return;
         const { addressTo, amount, message, keyword } = formData;
-        getEthereumContract();
+        const transactionContract =  getEthereumContract();
+        const parsedAmount = ethers.utils.parseEther(amount);
+        // transactionContract to call related function
+        await ethereum.request({    // request to send transaction  to the blockchain
+            method: "eth_sendTransaction",
+            params: [
+                {
+                    from: currentAccount,
+                    to: addressTo,
+                    // 5208 hexadecimal value of 0.00021 ether (21000 wei ->subunit of ether)
+                    gas: "0x5208", // 21000 wei
+                    // parseEther(amount) converts the amount to wei
+                    value: parsedAmount._hex,
+                }]
+        });
+        // store the transaction in the blockchain
+        const transactionHash =  await transactionContract.addToBlockchain(addressTo, parsedAmount, message, keyword);
+
+        setIsLoading(true);
+        // wait for the transaction to be mined
+        console.log(`loading transaction - ${transactionHash}`);
+        await transactionHash.wait();
+        setIsLoading(false);
+        // wait for the transaction to be mined
+        console.log(`success - ${transactionHash}`);
+        
+        const transactionCount = await transactionContract.getTransactionCount();
+
+        setTransactionCount(transactionCount.toNumber());
+
+
     } catch (error) {
+        console.log(error);
         throw new Error("No ethereum object found");
     }
     }
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,5 +1,5 @@
 import abi from './Transactions.json'
 
 export const contractABI=abi.abi;
-// from deployment: 0xfA8B7A4e26c488B33EceA5a8055CC30E97EBfAaB
+//its the contract address --> from deployment: 0xfA8B7A4e26c488B33EceA5a8055CC30E97EBfAaB
 export const contractAddress = '0xfA8B7A4e26c488B33EceA5a8055CC30E97EBfAaB';
