7d9232e1911a3eb0a70f7e78f575dc9399e5f051
“erhaneth” <gumusucb21@gmail.com>
2022-07-19 12:37:17 -0700

    Footer adjust

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,14 +4,14 @@ function Footer() {
   return (
     <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
       {/* <img src={image} alt="logo" className="" /> */}
-      <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
-        <div className="flex justify-center items-center">
+      <div className="w-full flex justify-center items-center my-4">
+        
           <img src={logo} alt="logo" className="w-32" />
-        </div>
+        
       </div>
       
 
-      <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
+      <div className="sm:w-[90%] w-full flex justify-center items-center mt-3">
         <p className="text-white flex justify-center ">© dbank2022</p>
       </div>
       <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -15,11 +15,11 @@ const useFetch = ({ keyword }) => {
       //const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${keyword.split(" ").join("")}&limit=1`);
         // const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=OhoITg1G03QmxEJeVp65GutiSVybqVkc&q=${keyword.split(" ").join("")}&limit=1&offset=0&rating=g&lang=en`);
         const response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${keyword.split(" ").join("")}&limit=1`);
-        console.log(response);
+        console.log(response.data.data);
         // const data  = await response.json();
         
   
-        setGifUrl(response.data[0]?.images?.downsized_medium.url);
+        setGifUrl(response.data.data[0]?.images?.downsized_medium.url);
         // console.log(gifUrl);
       } catch (error) {
         setGifUrl("https://www.omnisend.com/blog/wp-content/uploads/2016/09/funny-gifs-9.gif");
@@ -31,6 +31,7 @@ const useFetch = ({ keyword }) => {
     
   }, [keyword]);
 
+  
   return gifUrl;
 };
 
